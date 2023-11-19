4a55f4ff3d7be1b3bda23094ff85f75ae602dcee
kyto <mustafa.mahdi@kyto.com>
2023-11-19 13:24:35 +0100

    fix bug

--- src/components/Header.style.ts
+++ src/components/Header.style.ts
@@ -4,10 +4,14 @@ import { createStyles, makeStyles } from "@mui/styles";
 export const useHeaderStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      fontFamily: '"Fjalla One", sans-serif',
+      "&&": {
+        fontFamily: '"Fjalla One", sans-serif',
+      },
     },
     indicator: {
-      backgroundColor: `${theme.palette.secondary.main} !important`,
+      "&&": {
+        backgroundColor: `${theme.palette.secondary.main}`,
+      },
     },
   })
 );
