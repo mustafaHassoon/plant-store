97a8727d03075669591057536d278ce93185db0a
kyto <mustafa.mahdi@kyto.com>
2023-12-25 21:29:54 +0100

    fix routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import About from "./pages/About";
 import Store from "./pages/Store";
 import FindUs from "./pages/FindUs";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 export const App = () => {
   return (
@@ -17,10 +17,10 @@ export const App = () => {
         <Header />
         <div style={{ flex: 1 }}>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/store" element={<Store />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/find-us" element={<FindUs />} />
+            <Route path="#/home" element={<Home />} />
+            <Route path="#/store" element={<Store />} />
+            <Route path="#/about" element={<About />} />
+            <Route path="#/find-us" element={<FindUs />} />
           </Routes>
         </div>
         <Footer />
